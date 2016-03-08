27dfe95f88aa46da0a00f20268bbcd2babd058c1
R Dofitas <subterranean.toxin@gmail.com>
2016-03-08 22:30:54 +0800

    Additional behavior enhancements for dropdown component

--- components/Dropdown/Dropdown.jsx
+++ components/Dropdown/Dropdown.jsx
@@ -7,28 +7,65 @@ var Dropdown = {
   getInitialState: function() {
     return { isHidden: true };
   },
-  onClick: function() {
-    this.setState({ isHidden: !this.state.isHidden });
+  onClickOutside: function(evt) {
+    var currNode = evt.target,
+        isDropdown = false
+
+    do {
+      if(currNode.className.indexOf('dropdown-wrap') > -1) {
+        isDropdown = true
+        break
+      }
+
+      currNode = currNode.parentNode;
+    } while(currNode.tagName)
+
+    if(!isDropdown) {
+      this.setState({ isHidden: true })
+    }
+  },
+  onClick: function(evt) {
+    var openDropdowns = document.getElementsByClassName('Dropdown'),
+        dropdownClicked = new Event('dropdownClicked')
+
+    document.dispatchEvent(dropdownClicked)
+
+    this.setState({ isHidden: !this.state.isHidden })
+    evt.stopPropagation();
+  },
+  onClickOtherDropdown: function() {
+    this.setState({ isHidden: true })
+  },
+  componentDidMount: function() {
+    document.removeEventListener('click', this.onClickOutside)
+    document.removeEventListener('dropdownClicked', this.onClickOtherDropdown)
+
+    document.addEventListener('click', this.onClickOutside)
+    document.addEventListener('dropdownClicked', this.onClickOtherDropdown)
+  },
+  componentWillUnmount: function() {
+    document.removeEventListener('click', this.onClickOutside)
+    document.removeEventListener('dropdownClicked', this.onClickOtherDropdown)
   },
   render: function() {
     var pointerShadow = this.props.pointerShadow,
         noPointer = this.props.noPointer,
-        ndClasses = 'Dropdown';
+        ndClasses = 'Dropdown'
 
     if (pointerShadow) {
-      ndClasses += ' pointer-shadow';
+      ndClasses += ' pointer-shadow'
     }
 
     if (noPointer) {
-      ndClasses += ' pointer-hide';
+      ndClasses += ' pointer-hide'
     }
 
     if (this.state.isHidden) {
-      ndClasses += ' hide';
+      ndClasses += ' hide'
     }
 
     return (
-      <div onClick={ this.onClick }>
+      <div className="dropdown-wrap" onClick={ this.onClick } ref="Dropdown">
         { this.props.children[0] }
 
         <div className = {ndClasses}>
--- components/Dropdown/DropdownExamples.jsx
+++ components/Dropdown/DropdownExamples.jsx
@@ -1,19 +1,21 @@
 'use strict'
 
+require('./DropdownExamples.scss')
 var Dropdown = require('./Dropdown.jsx'),
     React    = require('react'),
     items = [
       'Review',
       'Web Arena',
       'Applet Arena',
-    ];
+    ]
 
 var DropdownExamples = {
   render: function() {
     var dom = 
-        <div className="dropdown-example-wrap">
-          <Dropdown className="Dropdown" pointerShadow>
-            <a className="dropdown-menu-header">Click Here</a>
+      <section>
+        <div className="dropdown-example full-width">
+          <Dropdown pointerShadow>
+            <a className="dropdown-menu-header">Full Width Dropdown</a>
             <ul className="dropdown-menu-list">
               {
                 items.map(function(link, i) {
@@ -23,6 +25,59 @@ var DropdownExamples = {
             </ul>
           </Dropdown>
         </div>
+
+        <div className="dropdown-example limited-width">
+          <Dropdown pointerShadow>
+            <a className="dropdown-menu-header">Limited Width Dropdown</a>
+            <ul className="dropdown-menu-list">
+              {
+                items.map(function(link, i) {
+                  return <li key={i}><a href="javascript:;">{link}</a></li>
+                })
+              }
+            </ul>
+          </Dropdown>
+        </div>
+
+        <div className="dropdown-example limited-width">
+          <Dropdown >
+            <a className="dropdown-menu-header">Limited Width Dropdown No Pointer Shadow</a>
+            <ul className="dropdown-menu-list">
+              {
+                items.map(function(link, i) {
+                  return <li key={i}><a href="javascript:;">{link}</a></li>
+                })
+              }
+            </ul>
+          </Dropdown>
+        </div>
+
+        <div className="dropdown-example limited-width">
+          <Dropdown noPointer>
+            <a className="dropdown-menu-header">Limited Width Dropdown No Pointer</a>
+            <ul className="dropdown-menu-list">
+              {
+                items.map(function(link, i) {
+                  return <li key={i}><a href="javascript:;">{link}</a></li>
+                })
+              }
+            </ul>
+          </Dropdown>
+        </div>
+
+        <div className="dropdown-example no-pointer">
+          <Dropdown noPointer pointerShadow>
+            <a className="dropdown-menu-header">Dropdown No Pointer</a>
+            <ul className="dropdown-menu-list">
+              {
+                items.map(function(link, i) {
+                  return <li key={i}><a href="javascript:;">{link}</a></li>
+                })
+              }
+            </ul>
+          </Dropdown>
+        </div>
+      </section>
       
     return dom;
   }
