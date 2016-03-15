2bb2eb6f4c6f01bab60eea445c2c73fcc3bb1df1
Reynard Dofitas <reynard2007@Reynards-MacBook-Pro.local>
2016-03-15 10:30:50 +0800

    Implement Navbar component

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+require('./Navbar.scss')
+
+import MenuBar from '../MenuBar/MenuBar'
+import React, {Component} from 'react'
+import SearchBar from '../SearchBar/SearchBar'
+import QuickLinks from '../QuickLinks/QuickLinks'
+import UserDropdownMenu from '../UserDropdownMenu/UserDropdownMenu'
+
+const primaryNavigationItems = [
+  {img: '../components/MenuBar/nav-community.svg', text: 'Community', link: 'javascript:;'},
+  {img: '../components/MenuBar/nav-compete.svg', text: 'Compete', link: 'javascript:;', selected: true},
+  {img: '../components/MenuBar/nav-learn.svg', text: 'Learn', link: 'javascript:;'}
+]
+
+const Navbar = {
+  render() {
+    return (
+      <div className="Navbar flex middle space-between">
+        <div className="topcoder-logo"></div>
+        <div className="search-bar-wrap">
+          <div className="icon-placeholder"></div>
+          <SearchBar />
+        </div>
+        <MenuBar items={primaryNavigationItems} mobileBreakPoint={767} orientation="horizontal" />
+        <div className="collapse-group">
+          <div className="icon-placeholder"></div>
+          <div className="quick-links-wrap"><QuickLinks /></div>
+          <UserDropdownMenu username="vic-tor" />
+        </div>
+      </div>
+    )
+  }
+}
+
+module.exports = React.createClass(Navbar)
