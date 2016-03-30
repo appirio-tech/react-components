541831df8073ac63828f8c33142c9f8531193e95
Amol Khanolkar <amolskh@gmail.com>
2016-03-30 22:34:20 +0530

    Updated code for Track Code

--- components/SubTrackDetails/SubTrackDetails.jsx
+++ components/SubTrackDetails/SubTrackDetails.jsx
@@ -9,7 +9,7 @@ const SubTrackDetails = (props) => {
         <div className={props.tracks.indexOf('develop') === -1 ? 'hidden' : 'verticalLine develop' }></div>
         <div className={props.tracks.indexOf('design') === -1 ? 'hidden' : 'verticalLine design' }></div> 
         <div className={props.tracks.indexOf('data science') === -1 ? 'hidden' : 'verticalLine dataScience' }></div> 
-        <div className="title">{props.name}</div>
+        <div className="title">{props.name} ({props.code})</div>
         </div>
     <p className={props.description ? '' : 'hidden' }>{props.description}</p>
     </div>
@@ -18,6 +18,7 @@ const SubTrackDetails = (props) => {
 
 SubTrackDetails.propTypes = {
   name: React.PropTypes.string,
+  code: React.PropTypes.string,
   description: React.PropTypes.string,
   tracks: React.PropTypes.array
 }
--- components/SubTrackDetails/SubTrackDetailsExample.jsx
+++ components/SubTrackDetails/SubTrackDetailsExample.jsx
@@ -9,14 +9,15 @@ const divStyle = {
 }
 
 const challengeDetails = {
-  name : 'First To Finish (F2F)',
+  name : 'First To Finish',
+  code : 'F2F',
   description : 'Specific challenge type where the first to submit a passable solution is the winner.',
   tracks : ['develop', 'design', 'data science']
 }
 
 const SubTrackDetailsExample = () => (
   <div style={divStyle}>
-    <SubTrackDetails name={challengeDetails.name} description={challengeDetails.description} tracks={challengeDetails.tracks}/>
+    <SubTrackDetails name={challengeDetails.name} code={challengeDetails.code} description={challengeDetails.description} tracks={challengeDetails.tracks}/>
   </div>
 )
 
