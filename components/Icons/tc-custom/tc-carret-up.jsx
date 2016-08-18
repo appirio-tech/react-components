3d3dcd78251ca1d06b108e10dd5b4000c50997c1
Victor George <vgeorge@appirio.com>
2016-08-18 08:35:02 -0700

    New icon organization

---
+++ components/Icons/tc-custom/tc-carret-up.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+// Carret Up Icon
+const tcCarretUp = (props) => {
+  const fill = props.fill || '#62AADC'
+  const height = props.height || '6'
+  const width = props.width || '10'
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" height={height} width={width} viewBox={ 0 0 height width} aria-labelledby="title">
+      <title id="title">tc-carret-up</title>
+      <path fill={fill} fill-rule="evenodd" d="M139.4782 187.2034c.3036-.2712.7623-.2712 1.066 0l4.478 4-1.0657 1.1932-3.9404-3.5284-3.95 3.5284-1.066-1.1932 4.4782-4z"/>
+    </svg>
+  )
+}
+
+tcCarretUp.propTypes = {
+	fill: React.PropTypes.string,
+	stroke: React.PropTypes.string,
+	height: React.PropTypes.number,
+	width: React.PropTypes.number
+}
+
+
+export default tcCarretUp
