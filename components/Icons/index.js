11e33b8f732fb07eb8e4b857788381e31f6b1108
vikasrohit <vikas.agarwal@appirio.com>
2016-08-27 09:15:58 +0530

    Fixed spell mistake

--- components/Icons/index.js
+++ components/Icons/index.js
@@ -135,7 +135,7 @@ export default {
   IconUsersDelete,
   IconUsersMultiple,
   IconUsersSingle,
-  IconSocialDribble,
+  IconSocialDribbble,
   IconSocialDropbox,
   IconSocialFacebook,
   IconSocialGithub,
