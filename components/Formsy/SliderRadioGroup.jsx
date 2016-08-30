8419e9956a9138b882573e33774ec04de7a494a4
Parth Shah <parth@topcoder.com>
2016-08-30 19:44:08 +0530

    fixing more form errors

--- components/Formsy/SliderRadioGroup.jsx
+++ components/Formsy/SliderRadioGroup.jsx
@@ -1,7 +1,6 @@
 'use strict'
 
 import React, { Component, PropTypes } from 'react'
-import PureRenderMixin from 'react-addons-pure-render-mixin'
 import cn from 'classnames'
 import _ from 'lodash'
 import { HOC as hoc } from 'formsy-react'
@@ -9,7 +8,6 @@ import { HOC as hoc } from 'formsy-react'
 class SliderRadioGroup extends Component {
   constructor(props) {
     super(props)
-    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
     this.onSlide = this.onSlide.bind(this)
   }
 
