33ff9aabf68fc146c4bdd3fbd75f15b53a735114
Thomas Kranitsas <kranitsasthomas@gmail.com>
2017-03-01 23:19:03 +0200

    Fix track icon for data science challenges and add rss link

--- components/TrackIcon/TrackIcon.jsx
+++ components/TrackIcon/TrackIcon.jsx
@@ -4,10 +4,10 @@ import './TrackIcon.scss';
 
 const TCO_URL = 'https://www.topcoder.com/tco';
 
-function TrackIcon ({track, subTrack, tcoEligible}) {
+function TrackIcon ({track, subTrack, tcoEligible, isDataScience}) {
   return (
     <span className="trackIcon">
-      <div className={track.toLowerCase() + ' main-icon'}>{Abbreviation[track][subTrack]}</div>
+      <div className={(isDataScience ? 'data_science' : track.toLowerCase()) + ' main-icon'}>{Abbreviation[track][subTrack]}</div>
       <a href={`${TCO_URL}`}>
       <div className={tcoEligible ? track.toLowerCase() + ' tco-icon' : 'hidden'}>TCO</div></a>
     </span>
