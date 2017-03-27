3fc033c62852d6a5a0824b9a7c8440ebde66d1d4
Vikas Agarwal <vikas.agarwal@topcoder.com>
2017-03-27 16:57:05 +0530

    Github issue#138, Remove console errors - removed warning for missing key for TiledRadioGroup component

--- components/Formsy/TiledRadioGroup.jsx
+++ components/Formsy/TiledRadioGroup.jsx
@@ -45,7 +45,7 @@ class TiledRadioGroup extends Component {
         </a>
       )
       return (
-        <span>
+        <span key={ idx }>
         {
           opt.disabled ?
           <Tooltip>
