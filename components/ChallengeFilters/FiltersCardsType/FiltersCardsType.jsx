2026efe29dcf7b9b23ea6a500fbdbc88b122ca28
Thomas Kranitsas <kranitsasthomas@gmail.com>
2017-03-31 00:58:43 +0300

    Remove SRM view

--- components/ChallengeFilters/FiltersCardsType/FiltersCardsType.jsx
+++ components/ChallengeFilters/FiltersCardsType/FiltersCardsType.jsx
@@ -10,14 +10,22 @@ import './FiltersCardsType.scss';
 const FiltersCardsType = ({setCardType, isCardTypeSet}) => {
     return (
       <div className="cards-type-col">
-        <a href="javascript:;" className={"challengs-tab " + (isCardTypeSet === 'Challenges' ? 'active' : '')}
-        onClick={() => setCardType('Challenges')}>Challenges</a>
-        <a href="javascript:;" className={"challengs-tab " +
-        (isCardTypeSet === 'SRMs' ? 'active' : '')}
-
-        onClick={() => setCardType('SRMs')}>SRMs</a>
+        <a href="javascript:;" className={"challengs-tab " + (isCardTypeSet === 'Challenges' ? 'active' : '')}>
+          Challenges
+        </a>
       </div>
     );
+
+    // return (
+    // <div className="cards-type-col">
+    //   <a href="javascript:;" className={"challengs-tab " + (isCardTypeSet === 'Challenges' ? 'active' : '')}
+    //     onClick={() => setCardType('Challenges')}>Challenges</a>
+    //   <a href="javascript:;" className={"challengs-tab " +
+    //     (isCardTypeSet === 'SRMs' ? 'active' : '')}
+    //
+    //     onClick={() => setCardType('SRMs')}>SRMs</a>
+    // </div>
+    // );
 };
 
 export default FiltersCardsType;
