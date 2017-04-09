c324fd80b5cf88274655307efa9b330573528c34
shubhu <shubhendu.saurabh@hotmail.com>
2017-04-09 09:27:34 +0530

    fixed appirio-tech/topcoder-app/issues/1175 changed winner lists in links

--- components/LeaderboardAvatar/LeaderboardAvatar.jsx
+++ components/LeaderboardAvatar/LeaderboardAvatar.jsx
@@ -6,12 +6,12 @@ const VISIBLE_CHARACTERS = 3
 
 function LeaderboardAvatar ({member}) {
   return (
-    <span className={`leaderboard-avatar ${member.position || member.isSmr ? 'dark-gray' : 'light-gray'}`}>
+    <a href={`//topcoder.com/members/${member.handle}`} className={`leaderboard-avatar ${member.position || member.isSmr ? 'dark-gray' : 'light-gray'}`}>
       {member.photoURL ? <img src={member.photoURL} className="member-icon"/> : member.handle.slice(0, VISIBLE_CHARACTERS)}
       <span className={member.position ? `placement placement-${member.position}` : 'hidden'}>
         {member.position}
       </span>
-    </span>
+    </a>
   )
 }
 
