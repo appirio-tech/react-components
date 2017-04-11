5fef459429ec412b9ef3a5249bc295d33dfceb16
shubhu <shubhendu.saurabh@hotmail.com>
2017-04-11 23:51:04 +0530

    changed to use props

--- components/LeaderboardAvatar/LeaderboardAvatar.jsx
+++ components/LeaderboardAvatar/LeaderboardAvatar.jsx
@@ -4,15 +4,25 @@ import './LeaderboardAvatar.scss'
 // Constants
 const VISIBLE_CHARACTERS = 3
 
-function LeaderboardAvatar ({member}) {
+function LeaderboardAvatar ({member, domain}) {
   return (
-    <a href={`//topcoder.com/members/${member.handle}`} className={`leaderboard-avatar ${member.position || member.isSmr ? 'dark-gray' : 'light-gray'}`}>
+    <a href={`//${domain}/members/${member.handle}`} className={`leaderboard-avatar ${member.position || member.isSmr ? 'dark-gray' : 'light-gray'}`}>
       {member.photoURL ? <img src={member.photoURL} className="member-icon"/> : member.handle.slice(0, VISIBLE_CHARACTERS)}
       <span className={member.position ? `placement placement-${member.position}` : 'hidden'}>
         {member.position}
       </span>
     </a>
   )
+}
+
+LeaderboardAvatar.propTypes = {
+  member: React.PropTypes.object,
+  domain: React.PropTypes.string,
+}
+
+LeaderboardAvatar.defaultProps = {
+  member: {},
+  domain: process.env.domain,
 }
 
 export default LeaderboardAvatar
