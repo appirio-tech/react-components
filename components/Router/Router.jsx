bd70370db9da8d90f04a45196e0155b28bff4b7d
Vikas Agarwal <vikas.agarwal@topcoder.com>
2017-11-09 11:46:52 +0530

    Fixed lint error

--- components/Router/Router.jsx
+++ components/Router/Router.jsx
@@ -1,7 +1,7 @@
 import React                         from 'react'
 import { Provider }                  from 'react-redux'
 import store                         from '../../store'
-import { BrowserRouter, Route, Switch, withRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 // import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 import ExampleApp                    from '../ExampleApp/ExampleApp.jsx'
