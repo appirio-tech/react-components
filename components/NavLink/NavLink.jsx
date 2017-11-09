b3f926147df0fbf6d713483757d7bb37e956a307
Vikas Agarwal <vikas.agarwal@topcoder.com>
2017-11-09 11:30:44 +0530

    upgrading react-components to react-router v4

--- components/NavLink/NavLink.jsx
+++ components/NavLink/NavLink.jsx
@@ -1,5 +1,5 @@
 import React, { PropTypes, Component } from 'react'
-import {withRouter, Link} from 'react-router'
+import {withRouter, Link} from 'react-router-dom'
 
 class NavLink extends Component {
   constructor(props) {
