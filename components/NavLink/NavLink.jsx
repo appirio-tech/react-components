7410804bb55ed78ce1c58fbf9a145dcf02ae030c
Vikas Agarwal <vikas.agarwal@topcoder.com>
2017-11-09 14:44:33 +0530

    Fixed our custom NavLink to work as expected for selected link after react router upgrade

--- components/NavLink/NavLink.jsx
+++ components/NavLink/NavLink.jsx
@@ -1,5 +1,6 @@
+import _ from 'lodash'
 import React, { PropTypes, Component } from 'react'
-import { NavLink as RNavLink} from 'react-router-dom'
+import { NavLink as RNavLink, matchPath} from 'react-router-dom'
 
 class NavLink extends Component {
   constructor(props) {
@@ -8,8 +9,16 @@ class NavLink extends Component {
 
   render() {
     const { to, content, target } = this.props
+    const { router } = this.context
     let classes = this.props.classes
     const attrs = { to }
+    if (to && router) {
+      const pathname = _.get(router, 'route.location.pathname', '')
+      const active = matchPath(pathname, { path: to }) != null
+      if (active) {
+        classes += ' selected'
+      }
+    }
     if (target || target !== '_self') {
       attrs.target = target
       if (attrs.target === '_blank') {
@@ -18,10 +27,14 @@ class NavLink extends Component {
     }
     return (
       <li className={classes}>
-        <RNavLink activeClassName="selected" {...attrs}>{content}</RNavLink>
+        <RNavLink activeClassName="active" {...attrs}>{content}</RNavLink>
       </li>
     )
   }
+}
+
+NavLink.contextTypes ={ 
+  router: PropTypes.object
 }
 
 NavLink.propTypes = {
